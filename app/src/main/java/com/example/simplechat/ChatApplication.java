60dce91ca95693164f6ba21e1222c6070869ca9f
Tejen Patel <git@tejen.net>
2020-07-05 18:55:02 -0700

    Complete lab with Polling Refresh using Handler

---
+++ app/src/main/java/com/example/simplechat/ChatApplication.java
@@ -0,0 +1,35 @@
+package com.example.simplechat;
+
+import android.app.Application;
+
+import com.parse.Parse;
+import com.parse.ParseObject;
+
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+
+public class ChatApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        ParseObject.registerSubclass(Message.class);
+
+        // Use for monitoring Parse OkHttp trafic
+        // Can be Level.BASIC, Level.HEADERS, or Level.BODY
+        // See http://square.github.io/okhttp/3.x/logging-interceptor/ to see the options.
+        OkHttpClient.Builder builder = new OkHttpClient.Builder();
+        HttpLoggingInterceptor httpLoggingInterceptor = new HttpLoggingInterceptor();
+        // Can be Level.BASIC, Level.HEADERS, or Level.BODY
+        httpLoggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+        builder.networkInterceptors().add(httpLoggingInterceptor);
+
+        // set applicationId and server based on the values in the Heroku settings.
+        // any network interceptors must be added with the Configuration Builder given this syntax
+        Parse.initialize(new Parse.Configuration.Builder(this)
+                .applicationId("simplechat-client") // should correspond to APP_ID env variable
+                .clientBuilder(builder)
+                .server("https://codepath-chat-lab.herokuapp.com/parse/").build());
+    }
+}
